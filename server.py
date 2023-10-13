b14f9ffc57f50e208c9830660d781f12a4295615
afodamz <afodamz07@gmail.com>
2023-10-13 01:13:35 +0100

    no django flask

---
+++ server.py
@@ -0,0 +1,33 @@
+import http.server
+import socketserver
+
+# Set the port on which you want to run the server
+port = 8000
+
+# Create a custom handler to serve HTML and JavaScript files
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'  # Default page
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+# Create the server with the custom handler
+with socketserver.TCPServer(("", port), CustomHandler) as httpd:
+    print(f"Serving at port {port}")
+    httpd.serve_forever()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python -m http.server
